38ad93a49842f21554d4e1aa6b98cdd60a333c4b
Илья <i.katishev@yandex.ru>
2025-02-21 23:03:02 +0300

    init commit

---
+++ log-archive.py
@@ -0,0 +1,54 @@
+import os
+import tarfile
+import parser_checker as pc #import module with overrided args via cli
+import datetime
+import shutil
+import sys
+
+#добавить
+
+def convert_size(size_bytes: int) -> str:
+    """Convert to human-readable file size"""
+    units = ["B", "KB", "MB", "GB", "TB", "PB"]
+    unit_index = 0
+    while size_bytes >= 1024 and unit_index < len(units) - 1:
+        size_bytes /= 1024.0
+        unit_index += 1
+    return f"{size_bytes:.2f} {units[unit_index]}"
+
+def create_fulname() -> str:
+    """Create full name of tar file according to scheme log_archive_<date>.tar.<compression>"""
+    date_time = str(datetime.datetime.now()).partition(".")[0].replace(" ", "_").replace("-", pc.args.delimiter).replace(":", pc.args.delimiter)
+    full_name_tarfile = "log_archive_"+date_time+pc.choose_ext()
+    return full_name_tarfile
+
+def make_tarfile(full_name_tarfile: str, target_path: str):
+    """"Create compressed tar and print its content if -v is given"""
+    try:
+        with tarfile.open(full_name_tarfile, "w:"+pc.choose_ext().split(".")[-1]) as tar:
+            tar.add(target_path, arcname=".")
+    except FileNotFoundError as e:
+        print(f"ERROR: {e}")
+        sys.exit(1)
+        
+    if pc.args.verbose == True:
+        with tarfile.open(full_name_tarfile, "r") as tar:
+            for member in tar.getmembers():
+                print(member.name)
+
+    return(f"{full_name_tarfile} size is {convert_size(os.stat(full_name_tarfile).st_size)}")
+
+def main():
+    """main func for calling others"""
+    tar_file = create_fulname()
+    print(make_tarfile(tar_file, pc.args.target))
+
+
+    print(os.stat(tar_file))
+
+    shutil.move(tar_file, pc.args.backup)
+
+
+if __name__ == "__main__":
+    main()
+    #os.stat(tar_file)
---
+++ parser_checker.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+import sys
+
+#create parser obj
+parser = argparse.ArgumentParser(
+                    prog='log-archive.py',
+                    description='log-archive is utility for making backups of /var/log to /var/backup path with timestamp',
+                    usage='%(prog)s [options]',
+                    epilog='Project for roadmap.sh https://docs.python.org/3/library/argparse.html')
+
+#add a few args
+parser.add_argument("-c", "--compress", 
+                    help="option to %(prog)s for compress type (default: %(default)s)", 
+                    default="z", 
+                    metavar="")
+
+parser.add_argument("-d", 
+                    "--delimiter", 
+                    help="option to %(prog)s for delimiter for timestamp (default: %(default)s)", 
+                    default="-", 
+                    metavar="")
+
+parser.add_argument("-v", 
+                    "--verbose", 
+                    help="option to %(prog)s for showing output during running command (default: %(default)s)", 
+                    default=False,
+                    action="store_true")
+
+parser.add_argument("-t", 
+                    "--target",
+                     help="option to %(prog)s for path of archived folder (default: %(default)s)",
+                     default="venv", 
+                     metavar="")
+
+parser.add_argument("-b", 
+                    "--backup", 
+                    help="option to %(prog)s for path of folder to save backups (default: %(default)s)", 
+                    default="log-archive", 
+                    metavar="")
+
+parser.add_argument('--version', 
+                    action='version', 
+                    version='%(prog)s 0.1v')
+
+#create tuple of args
+args = parser.parse_args()
+
+
+#Реализовать проверки аргументов, обработку ошибок чтобы в log-archive.py уже всё было заебись. Проверку прав на чтение и тд
+def choose_ext() -> str:
+    """return tar extesnion corresponding to compression type"""
+    try:
+        if args.compress == "j":
+            return ".tar.bz2"
+        elif args.compress == "J":
+            return ".tar.xz"
+        elif args.compress == "z":
+            return ".tar.gz"
+        else:
+            raise ValueError(f"No available key for tar compression: {args.compress}")
+    except ValueError as e:
+        print(f"ERROR: {e}")
+        # Дополнительные действия: логирование, выход из программы и т.д.
+        sys.exit(1)
+
+try:
+    if not os.path.isdir(args.target):
+        raise FileExistsError(f"No found directory for archive {args.target}")
+except FileExistsError as e:
+    print(f"ERROR: {e}")
+    sys.exit(1)
+
+if not os.path.isdir(args.backup):
+    os.makedirs(args.backup)
